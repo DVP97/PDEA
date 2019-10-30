9c18d1a94561c3bb3eb0209378c45c6c4ef9ecd1
DVP97 <dvduckee@gmail.com>
2019-10-30 14:29:46 +0100

    Input Usuario
    Tomar datos introducidos en el textfield de Usuario, pasar a mayúsculas las letras y lanzar una excepción a través de una ventana de aviso en caso de que la longitud del string introducido sea distinto a 9 (longitud del dni).

--- src/application/ControladorInterfaz.java
+++ src/application/ControladorInterfaz.java
@@ -2,8 +2,13 @@ package application;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javax.swing.JOptionPane;
+
 import com.gluonhq.charm.glisten.control.TextField;
 import com.jfoenix.controls.JFXButton;
+
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -46,12 +51,69 @@ public class ControladorInterfaz implements Initializable{
 	
 	@Override
 	public void initialize(URL location, ResourceBundle reosurces) {
-	//no hace falta hacer nada aqu�	
+	//no hace falta hacer nada aqu�	
 	}
 		
-	public void aceptado(ActionEvent event) {
-		System.out.println("Bienvenido a PDEA.");
+	//Acciones ejecutadas al pulsar el Boton Aceptar
+	public void pressBtnAceptar(ActionEvent event) {
+		
+		try {
+			comprobarInputUser();
+			if (comprobarInputUser() == false) {
+				throw new ExcepcionUser(01);
+			}
+			
+		}
+		catch(ExcepcionUser loginfailure){
+			JOptionPane.showMessageDialog(null, "El usuario debe estar compuesto por 8 digitos y una letra.", "Aviso", JOptionPane.WARNING_MESSAGE);
+		}
+		
+	}
+	//Acciones ejecutadas al pulsar el Boton Cancelar
+	public void pressBtnCancelar(ActionEvent event) {
+		System.out.println("Saliendo de la aplicación. Hasta pronto.");
+		System.exit(0);
+	}	
+
+	public boolean comprobarInputUser() {		
+		// capturar texto  del TextField de usuario y convierte a mayus.
+		String inputUser = txtInputUsuario.getText().toUpperCase();
+		
+		System.out.println("Usuario introducido: "+inputUser);
+		
+		
+		/*
+		//comprobar caracter a caracter si es un numero
+		for(int i=0; i<inputUser.length()-1; i++){
+		 
+			System.out.println(inputUser.charAt(i));
+		//	Double a = Double.parseDouble(inputUser.charAt(i);
+			if (inputUser.charAt(i) == instanceof java.lang.Integer){
+				System.out.println("error");
+				return false;
+			}
+		}
+		*/
+		
+		//comprueba la longitud del string inputUser
+		if(inputUser.length()!=9) {
+			return false;
+		}
+		else {
+			System.out.println("Usuario válido.");
+			return true;
+		}
 		
 		
 	}
+	
+	public class ExcepcionUser extends Exception{
+	     
+	    private int codigoError;
+	    
+	    public ExcepcionUser(int codigoError){
+	        this.codigoError=codigoError;
+	    }
+	}
+
 }
