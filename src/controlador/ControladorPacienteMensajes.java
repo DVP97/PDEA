43a1985f2b73d0aff99a3a77bce89fecaf204a5b
sofiamartinezparada <55553291+sofiamartinezparada@users.noreply.github.com>
2019-11-24 11:30:59 +0100

    Mensajes

--- src/controlador/ControladorPacienteMensajes.java
+++ src/controlador/ControladorPacienteMensajes.java
@@ -8,17 +8,47 @@ import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.BorderPane;
 import modelo.Paciente;
 
 public class ControladorPacienteMensajes implements Initializable{
 
-	
+	@FXML
+	private BorderPane BorderPaneGlobal;
 	
 	@FXML
 	private JFXButton btnEnviar; 
 	
 	@FXML
+	private JFXButton btnVolver;
+	
+	@FXML
     private Label campoPaciente;
+	
+	@FXML
+	private Tab tabRecibidos;
+	
+	@FXML
+	private TitledPane structMensajeRecib;
+	
+	@FXML
+	private Label textoMensajeRecib;
+	
+	@FXML
+	private Tab tabEnviados;
+	
+	@FXML
+	private TitledPane structMensajeEnv;
+	
+	@FXML
+	private Label textoMensajeEnv;
+	
+	@FXML
+	private TextArea campoEscritura;
+	
 	
 	private static Paciente pacienteActual = new Paciente();
 	
