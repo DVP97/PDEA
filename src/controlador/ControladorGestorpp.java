af72959c712bc811abded94ab4b4edc8a73239ae
Marina <m.mucientes@usp.ceu.es>
2020-05-14 19:54:20 +0200

    Conocerte

---
+++ src/controlador/ControladorGestorpp.java
@@ -0,0 +1,53 @@
+package controlador;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+
+public class ControladorGestorpp {
+
+    @FXML
+    private Label campoGestor;
+
+    @FXML
+    private JFXComboBox<?> comboRol;
+
+    @FXML
+    private JFXButton btnRegistrar;
+
+    @FXML
+    private JFXButton btnModificar;
+
+    @FXML
+    private JFXButton btnRelacionar;
+
+    @FXML
+    private JFXButton btnEliminar;
+
+
+    @FXML
+    void pressBtnEliminar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pressBtnModificar(ActionEvent event) {
+
+    }
+
+    //En esta acción se abre la ventana de registro
+    @FXML
+    void pressBtnRegistrar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pressBtnRelacionar(ActionEvent event) {
+
+    }
+
+}
