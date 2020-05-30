3016a4483d882702233fad10a461275eceb29d3e
sofiamartinezparada <sofia.martinez.parada@outlook.es>
2020-05-30 17:42:13 +0200

    no se que pasa

--- src/modelo/Medico.java
+++ src/modelo/Medico.java
@@ -3,16 +3,29 @@ package modelo;
 
 public final class Medico extends Usuario{
 	
-	public Medico(String dni, String nombre, String apellidos, String telefono, String contrasena) {
+	boolean isGestor;
+	
+	public Medico(String dni, String nombre, String apellidos, String telefono, String contrasena, boolean isGestor) {
 		super(dni, nombre, apellidos, telefono, contrasena);
+		this.isGestor = isGestor;
 	}
 	public Medico() {
 		super();
+		this.isGestor = false;
 	}
 	
 	//GETTERS
 	public String getNombreCompleto() {
 		String nombreCompleto =getNombre() + " " + getApellidos();
 		return nombreCompleto;
+	}
+	
+	public boolean isGestor() {
+		return isGestor;
+	}
+	
+	//SETTERS
+	public void setGestor(boolean isGestor) {
+		this.isGestor = isGestor;
 	}
 }
